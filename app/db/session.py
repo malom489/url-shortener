a6082602105e999e09c4a0779f3e1d1d189f4082
malom489 <malommwiti1.com>
2026-09-05 00:14:43 +0300

    milestone 1:database sessiona and connection layer

---
+++ app/db/session.py
@@ -0,0 +1,33 @@
+from typing import AsyncGenerator
+from sqlalchemy import URL
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine 
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config  import settings
+
+#engine
+engine=create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    pool_size=20,
+    max_overflow=10,
+)
+
+#session
+SessionLocal=sessionmaker(
+    
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,)
+
+Base=declarative_base()
+
+async def get_db()  -> AsyncGenerator [AsyncSession, None]:
+    async with SessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception as e:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
